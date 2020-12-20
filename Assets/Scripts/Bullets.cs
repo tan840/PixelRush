d24c0f0a47f04a0595a110f51477fd0cac79cdc9
Tanvir Ahmed <42252274+tan840@users.noreply.github.com>
2020-12-20 17:49:21 +0600

    First Contribution

---
+++ Assets/Scripts/Bullets.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+    public float bulletSpeed;
+    public float bulletLifetime;
+
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        StartCoroutine(DeactiveBulletCoroutine());
+    }
+    
+    IEnumerator DeactiveBulletCoroutine()
+    {
+        yield return new WaitForSeconds(bulletLifetime);
+        DeactiveBullet();
+    }
+    void DeactiveBullet()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * bulletSpeed * Time.deltaTime);
+    }
+}
